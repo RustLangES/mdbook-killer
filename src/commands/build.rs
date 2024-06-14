430005ff2112ff0ef2162fd9883bf46a406a2abf
Phosphorus <fernando.n.p.d.moscu@gmail.com>
2024-06-14 13:29:17 -0300

    feat: inject html into the template

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -4,7 +4,11 @@ use crate::models::lang_config::LanguageConfig;
 use crate::models::Chapter;
 use crate::renderer::ssg::Ssg;
 use anyhow::{anyhow, Context, Result};
-use std::fs::{self, ReadDir};
+use leptos::html::AnyElement;
+use leptos::leptos_dom::{ComponentRepr, Element};
+use leptos::ssr::render_to_string;
+use leptos::{component, document, view, Children, HtmlElement, IntoView};
+use std::fs::{self, read_to_string, ReadDir};
 use std::path::Path;
 
 use gray_matter::engine::YAML;
@@ -29,6 +33,7 @@ pub async fn execute(default_language: Option<String>, languages: Option<Vec<Str
     std::fs::write("./out/book/style.css", CSS_FILE)?;
     
     let mut chapters = Vec::with_capacity(10);
+    let custom_component = read_to_string("./theme/chapter.html").ok();
     
     for lang in languages {
         let chapter_folder = fs::read_dir(format!("./src/{}", lang))?;
@@ -48,7 +53,7 @@ pub async fn execute(default_language: Option<String>, languages: Option<Vec<Str
         }
         let ssg = Ssg::new(out);
 
-        _ = generate_chapters(&ssg, chapters.clone()).await;
+        _ = generate_chapters(&ssg, chapters.clone(), custom_component.clone()).await;
     }
     _ = generate_homepage(&ssg, chapters).await;
 
@@ -56,7 +61,18 @@ pub async fn execute(default_language: Option<String>, languages: Option<Vec<Str
     Ok(())
 }
 
-async fn generate_chapters<'a>(ssg: &Ssg<'a>, chapters: Vec<Chapter>) -> Result<(), Box<dyn std::error::Error>> {
+#[component]
+pub fn CustomComponent(
+    #[prop(into)] content: String,
+    #[prop(into)] cosa: String
+) -> impl IntoView {
+    let content = content.replace("{cosa}", &cosa);
+    view!{<div inner_html={content}></div>}
+}
+
+async fn generate_chapters<'a>(ssg: &Ssg<'a>, chapters: Vec<Chapter>, custom_component: Option<String>) -> Result<(), Box<dyn std::error::Error>> {
+    
+
     let chapters_clone = chapters.clone(); 
     for chapter in chapters {
         let path = chapter.slug.clone().unwrap();
@@ -64,11 +80,17 @@ async fn generate_chapters<'a>(ssg: &Ssg<'a>, chapters: Vec<Chapter>) -> Result<
 
         let chapter_prop = Some(chapter.clone());
         let chapters_prop = chapters_clone.clone(); 
+        // let custom_component = element.clone().unwrap();
 
-        ssg.gen(path, || Homepage(HomepageProps{
-            chapter:  chapter_prop,
-            chapters: chapters_prop
+        ssg.gen(path, || CustomComponent(CustomComponentProps{
+            content: "<div><p>Hola {cosa}</p></div>".to_string(),
+            cosa: "algoooo".to_string()
         })).await?;
+
+        // ssg.gen(path, || Homepage(HomepageProps{
+        //     chapter:  chapter_prop,
+        //     chapters: chapters_prop
+        // })).await?;
     }
 
     Ok(())
